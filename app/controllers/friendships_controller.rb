3c203a3d3210ffcebe1c290a1fb3ebc6ab9a17f6
uwadonat <uwadonat@gmail.com>
2021-04-15 04:33:14 -0400

    remove sent and pending request if empty

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,23 +4,23 @@ class FriendshipsController < ApplicationController
     def create
         @user = User.find(params[:format])
         @friendship = current_user.friendships.create(friend_id: params[:format])
-        if @friendship.save
-           redirect_to user_path(params[:format])
-        else
-           redirect_to users_path
-        end
+        @friendship.save
+        redirect_back(fallback_location: root_path) 
+       
     end
 
     def update
         @friendship = Friendship.find(params[:id])
         @friendship.confirmed = true
         @friendship.save
-        redirect_to users_path
+        redirect_back(fallback_location: root_path) 
+     
     end
 
     def destroy
         @friendship = Friendship.find(params[:id])
         @friendship.destroy
-        redirect_to users_path
+        redirect_back(fallback_location: root_path) 
+       
     end
 end
